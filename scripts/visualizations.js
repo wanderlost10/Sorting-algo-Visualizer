af0f792beb9c4c46e9f22e71a25d0342d22f96d6
wanderlost10 <116659573+wanderlost10@users.noreply.github.com>
2024-11-29 00:43:16 +0530

    Update visualizations.js

--- scripts/visualizations.js
+++ scripts/visualizations.js
@@ -1,11 +1,3 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
-
-//We only have to change background-color and height of the sorting element.
 
 var speed=1000;
 
@@ -57,9 +49,3 @@ function enable_buttons()
     },c_delay+=delay_time);
 }
 
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
